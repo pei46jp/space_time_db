fdd0d44319ad649a4aa91f85789611973274d305
peipeimuds <s1922027@stu.musashino-u.ac.jp>
2021-07-26 12:09:32 +0900

    remake layout

--- final/final/settings.py
+++ final/final/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_z-%43q^l*0fu*))h(^n-anm-7ip76r-!kya2b-y@v#u=_97vm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
